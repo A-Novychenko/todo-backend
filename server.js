7607df4e3b65698e3815f356333bf74cab92a7ac
Aleksandr Novychenko <A.Novychenko@outlook.com>
2023-06-12 20:34:48 +0300

    set up a server

--- server.js
+++ server.js
@@ -1,9 +1,52 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const logger = require("morgan");
+
 require("dotenv").config();
 
 const app = express();
-const {PORT = 3001} = process.env;
+const {PORT = 3001, DB_HOST} = process.env;
 
-app.listen(3000, () => {
-  console.log(`Server running. Use our API on port: ${PORT}`);
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+
+app.use((_, res, __) => {
+  res.status(404).json({
+    status: error,
+    code: 404,
+    message: "Use api on routes: /api/todos",
+    data: "Not found",
+  });
 });
+
+app.use((err, req, res, next) => {
+  const {status = 500, message = "Server error"} = err;
+  const {statusText, codeErr, messageDescr, dataDescr} = createErrorReq(
+    status,
+    message
+  );
+
+  res.status(status).json({
+    status: statusText,
+    code: codeErr,
+    message: messageDescr,
+    data: dataDescr,
+  });
+});
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log(`Database connection successful`);
+    app.listen(3000, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`);
+    });
+  })
+  .catch((e) => {
+    console.log(`Server not running. Error message: ${err.message}`);
+    process.exit(1);
+  });
